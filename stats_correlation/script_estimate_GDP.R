f4fccbaba40c0e5cee5a20708e720dbd624c0887
Charles De Dampierre <charles.de-dampierre@hec.edu>
2023-07-05 11:46:40 +0200

    add notebooks

--- stats_correlation/script_estimate_GDP.R
+++ stats_correlation/script_estimate_GDP.R
@@ -22,3 +22,17 @@ model_1 <- lme(gdp_per_capita ~ score + year,
 model_cor_1 <- update(model_1, correlation = corAR1(form = ~ year | region_name))
 
 summary(model_cor_1)
+
+residuals <- resid(model_cor_1)
+
+# Calculate the total sum of squares
+total_sum_of_squares <- sum((data$gdp_per_capita - mean(data$gdp_per_capita))^2)
+
+# Calculate the residual sum of squares
+residual_sum_of_squares <- sum(residuals^2)
+
+# Calculate the R-squared value
+r_squared <- 1 - (residual_sum_of_squares / total_sum_of_squares)
+
+# Print the R-squared value
+print(paste("R-squared:", r_squared))
