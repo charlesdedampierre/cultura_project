b9fd98f8b5dd15ac35971a09362c88926881a8ec
Charles De Dampierre <charles.de-dampierre@hec.edu>
2024-03-08 20:39:37 +0100

    change occupation model

--- unseen_species_model/run_occupation_covariate.py
+++ unseen_species_model/run_occupation_covariate.py
@@ -5,7 +5,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
-
 df = pd.read_csv("data/df_indi_works_occupations.csv", index_col=0)
 print(df.occupation.value_counts())
 
@@ -13,24 +12,39 @@ regions = pd.read_csv(
     "data/ENS - Cultural Index - Countries Databases - region_level.csv"
 )
 regions["region_name"][regions["region_name"] == "Slav world"] = "East Slavic"
-
 regions = regions[regions["level"] == 2]
+regions = regions[regions["region_name"] != "Balkans"]
+regions = regions[
+    regions["region_name"] != "Eastern Europe"
+]  # remove because it takes East Slavic and Central Europe
 regions = set(regions["region_name"])
 df = df[df["region_name"].isin(regions)]
 
 df["century"] = df["decade"].round(-2)
-df = df[
-    ~((df["region_name"] == "Italy") & (df["decade"] < 500))
-]  # Fix issue with Italy and Rome that overlaps
-# df = df[df['decade']>=1800]
+df = df[~((df["region_name"] == "Italy") & (df["decade"] < 500))]
+df = df[~((df["region_name"] == "Italy") & (df["decade"] < 500))]
+df = df[~((df["region_name"] == "Portugal") & (df["decade"] < 500))]
+df = df[~((df["region_name"] == "Spain") & (df["decade"] < 500))]
+df = df[~((df["region_name"] == "Arabic world") & (df["decade"] < 500))]
+df = df[~((df["region_name"] == "Balkans") & (df["decade"] < 500))]
+df = df[~((df["region_name"] == "France") & (df["decade"] < 500))]
+df = df[~((df["region_name"] == "United Kingdom") & (df["decade"] < 500))]
+df = df[~((df["region_name"] == "Low countries") & (df["decade"] < 500))]
+df = df[~((df["region_name"] == "East Slavic") & (df["decade"] < 500))]
+df = df[~((df["region_name"] == "Central Europe") & (df["decade"] < 500))]
+df = df[~((df["region_name"] == "Eastern Europe") & (df["decade"] < 500))]
+df = df[~((df["region_name"] == "German world") & (df["decade"] < 500))]
+df = df[~((df["region_name"] == "Latin world") & (df["decade"] > 500))]
+
+# Avoid overlapping with Antiquity
 
 df["count"] = df["count_works"]
 df_m = df.copy()
 df_m = df_m[df_m["count"].isin({0, 1, 2})]  # Not more ?
 df_m["y"] = df_m["count"].map({0: 0, 1: 0, 2: 1})
 
-# about 6% of women compared to men
-df_m = df_m[df_m["decade"] < 1870]
+# # about 6% of women compared to men
+# df_m = df_m[df_m["decade"] < 1870]
 
 # knots
 num_knots = 10
@@ -38,10 +52,9 @@ knots = np.linspace(df["century"].min(), df["century"].max(), num_knots)
 iknots = knots[1:-1]
 
 sample = df_m.copy()
-# sample = sample.sample(2000, random_state=42)
+sample = sample.sample(5000, random_state=42)
 
 models = {}
-sample_base = sample.drop("occupation", axis=1)
 
 base_model = bmb.Model(
     "y ~ 1",  # variance spline and variance intercept model
@@ -51,6 +64,18 @@ base_model = bmb.Model(
         "Intercept": bmb.Prior("Normal", mu=0, sigma=2.5),
     },
 )
+
+base_model_fitted = base_model.fit(
+    draws=1000,
+    chains=4,
+    inference_method="nuts_numpyro",
+    idata_kwargs={"log_likelihood": True},
+)  # important to run faster and sample more efficiently
+
+models["y ~ 1"] = base_model_fitted
+
+az.waic(models["y ~ 1"])
+
 
 variance_model = bmb.Model(
     "y ~ 1 + (bs(decade, knots=iknots, intercept=True)|region_name)",  # variance spline and variance intercept model
@@ -65,6 +90,20 @@ variance_model = bmb.Model(
         ),
     },
 )
+
+variance_model_fitted = variance_model.fit(
+    draws=1000,
+    chains=4,
+    inference_method="nuts_numpyro",
+    idata_kwargs={"log_likelihood": True},
+)  # important to run faster and sample more efficiently
+
+models["y ~ 1 + (bs(decade, knots=iknots, intercept=True)|region_name)"] = (
+    variance_model_fitted
+)
+
+az.waic(models["y ~ 1 + (bs(decade, knots=iknots, intercept=True)|region_name)"])
+
 
 # OCCUPATION MODEL
 occupation_model = bmb.Model(
@@ -81,6 +120,22 @@ occupation_model = bmb.Model(
         ),
     },
 )
+
+occupation_model_fitted = occupation_model.fit(
+    draws=1000,
+    chains=4,
+    inference_method="nuts_numpyro",
+    idata_kwargs={"log_likelihood": True},
+)  # important to run faster and sample more efficiently
+
+models["y ~ (bs(decade, knots=iknots, intercept=True)|region_name) + occupation"] = (
+    occupation_model_fitted
+)
+
+az.waic(
+    models["y ~ (bs(decade, knots=iknots, intercept=True)|region_name) + occupation"]
+)
+
 
 # OCCUPATION MODEL
 occupation_model_variance = bmb.Model(
@@ -101,36 +156,29 @@ occupation_model_variance = bmb.Model(
     },
 )
 
-occupation_model_fitted = occupation_model.fit(
+occupation_model_variance_fitted = occupation_model_variance.fit(
     draws=1000,
     chains=4,
     inference_method="nuts_numpyro",
     idata_kwargs={"log_likelihood": True},
 )  # important to run faster and sample more efficiently
 
-models["occupation-model"] = occupation_model_fitted
-az.waic(models["occupation-model"])
+models[
+    "y ~ (bs(decade, knots=iknots, intercept=True)|region_name)) + (1|occupation)"
+] = occupation_model_variance_fitted
+az.waic(
+    models[
+        "y ~ (bs(decade, knots=iknots, intercept=True)|region_name)) + (1|occupation)"
+    ]
+)
 
-# base_model = bmb.Model(
-#     "y ~ bs(decade, knots=iknots, intercept=True) + (1|region_name)",
-#     sample_base[["decade", "region_name", "y"]],
-#     family="bernoulli",
-#     priors=priors_base_model,
-# )
-
-# base_model_fitted = base_model.fit(
-#     draws=1000,
-#     chains=4,
-#     inference_method="nuts_numpyro",
-#     idata_kwargs={"log_likelihood": True},
-# )  # important to run faster and sample more efficiently
 
 # GET THE ALPHA FOR EVERY OCCUPATION
 
 forest_plot = az.plot_forest(
-    data=occupation_model_fitted,
+    data=occupation_model_variance_fitted,
     figsize=(6, 4),
-    var_names=["occupation"],
+    var_names=["1|occupation"],
     r_hat=True,
     combined=True,
     textsize=8,
@@ -141,11 +189,7 @@ fig = forest_plot[0].get_figure()
 fig.set_size_inches(8, 4)  # Adjust size as needed
 fig.tight_layout()
 
-fig.savefig("results/occupation/forest_plot.png")
-
-
-models["base-model"] = base_model_fitted
-az.waic(models["base-model"])
+fig.savefig("results/occupation/forest_plot_variance.png")
 
 
 # Comparison plot
@@ -278,35 +322,38 @@ def get_unseen_numbers(df, model, model_fitted, region="Chinese world"):
     return table
 
 
-# GET THE RESULTS FOR THE CLASSICAL UNSEEN MODEL
-final_table = []
-for region in tqdm(sample_base.region_name.unique()):
-    table = get_unseen_numbers(
-        sample_base, base_model, base_model_fitted, region=region
-    )
-    final_table.append(table)
+sample_base = sample.drop("occupation", axis=1)
 
-df_final_table = pd.concat([x for x in final_table])
-df_final_table["model_type"] = "base_model"
-df_count = (
-    sample_base.groupby(["region_name", "decade"])["individual_wikidata_id"]
-    .count()
-    .rename("count_cps")
-    .reset_index()
-)
-df_final_table = pd.merge(df_final_table, df_count, on=["region_name", "decade"])
-df_final_table.to_csv("results/occupation/unseen_data.csv")
+
+# # GET THE RESULTS FOR THE CLASSICAL UNSEEN MODEL
+# final_table = []
+# for region in tqdm(sample_base.region_name.unique()):
+#     table = get_unseen_numbers(
+#         sample_base, base_model, base_model_fitted, region=region
+#     )
+#     final_table.append(table)
+
+# df_final_table = pd.concat([x for x in final_table])
+# df_final_table["model_type"] = "base_model"
+# df_count = (
+#     sample_base.groupby(["region_name", "decade"])["individual_wikidata_id"]
+#     .count()
+#     .rename("count_cps")
+#     .reset_index()
+# )
+# df_final_table = pd.merge(df_final_table, df_count, on=["region_name", "decade"])
+# df_final_table.to_csv("results/occupation/unseen_data.csv")
 
 
 # GET THE RESULTS FOR THE CLASSICAL UNSEEN MODEL FOR OCCUPATION
 
-final_table = []
-for region in sample.region_name.unique():
-    table = get_unseen_numbers_occupation(
-        sample, occupation_model, occupation_model_fitted, region=region
-    )
-    final_table.append(table)
+# final_table = []
+# for region in sample.region_name.unique():
+#     table = get_unseen_numbers_occupation(
+#         sample, occupation_model, occupation_model_fitted, region=region
+#     )
+#     final_table.append(table)
 
-df_final_table = pd.concat([x for x in final_table])
-df_final_table["model_type"] = "occupation_model"
-df_final_table.to_csv("results/occupation/unseen_data_occupation.csv")
+# df_final_table = pd.concat([x for x in final_table])
+# df_final_table["model_type"] = "occupation_model"
+# df_final_table.to_csv("results/occupation/unseen_data_occupation.csv")
